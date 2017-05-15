80d11e75aa9eb2ef71b673b044a97252d51c0950
DmitryBykin <bykin2002@mail.ru>
2017-05-15 17:07:15 +0400

    Добавлены карточки товаров для Черемшан и Поиск-НЕТ. Изменен порядок следования пунктов с услугами

---
+++ Gruntfile.js
@@ -0,0 +1,148 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-csso");
+  grunt.loadNpmTasks("grunt-webp");
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 2 versions"
+            ]})
+          ]
+        },
+        src: "css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      styles: {                
+          files: ["less/**/*.less"],
+          tasks: ["less", "postcss", "csso"]  
+        },
+      csso: {
+        files: ["css/styles.css"],
+        tasks: ["csso"]  
+      },
+      webp: {
+        files: ["img/**/*.{png,gif,jpg}"],
+        tasks: ["webp"]
+      }
+      //,
+      //style: {
+      //  files: ["img/**/*.{png,gif,jpg}"],
+      //  tasks: ["webp"]
+      //}      
+      //,
+      //js: {
+      //  files: ["js/**/*.js"],
+      //  tasks: ["uglify"]
+      //}
+      //,
+      //webpack: {
+      //  files: ["images/**/*.{png,gif,jpg}"],
+      //  tasks: ["webp"]
+      //}
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "css/styles.min.css": ["css/styles.css"]
+        }
+      }
+    },
+
+    webp: {
+      files: {
+        expand: true,
+        src: 'img/*.{png,gif,jpg}'
+        //dest: 'images/*'
+        //cwd: 'source-images/*.png'
+      },
+      options: {
+        binpath: require('webp-bin').path,
+        preset: 'photo',
+        verbose: true,
+        quality: 80,
+        alphaQuality: 80,
+        compressionMethod: 6,
+        segments: 4,
+        psnr: 42,
+        sns: 50,
+        filterStrength: 40,
+        filterSharpness: 3,
+        simpleFilter: true,
+        partitionLimit: 50,
+        analysisPass: 6,
+        multiThreading: true,
+        lowMemory: false,
+        alphaMethod: 0,
+        alphaFilter: 'best',
+        alphaCleanup: true,
+        noAlpha: false,
+        lossless: false
+      }
+    },
+
+    autoprefixer: {
+        //указывам файл в котором нужно проставить префиксы, он сам его перезапишит
+        no_dest: {
+            src: "css/styles.css"
+        }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'js/script.min.js': ['js/*.js']
+        }
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);   
+  //grunt.registerTask('default', 'webp');
+};
